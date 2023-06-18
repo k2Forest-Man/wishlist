5022ace540ff33b71e454ea2c5ad541a2ffcc1d2
k2Forest-Man <LesnoiChelovek@yandex.ru>
2023-06-18 23:16:30 +0300

    написал код для отображения модалки Авторизация, теперь зарегистрированный пользователь может войти

--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -52,8 +52,8 @@ export const renderNavigation = () => {
           alert(error.message)
         }
       }
-    })
-  })
+    });
+  });
 
   // <button class="nav__btn btn">Войти</button>
   const buttonLogin = createElement('button', {
@@ -62,7 +62,42 @@ export const renderNavigation = () => {
   });
 
   buttonLogin.addEventListener('click', () => {
-    console.log('Клик был по кнопке Войти');
+    renderModal({
+      title: 'Авторизация',
+      description: 'Введите ваши данные для входа в личный кабинет',
+      btnSubmit: 'Авторизоваться',
+      async submitHandler(event) {
+        const formData = new FormData(event.target);
+        const credentials = {
+          login: formData.get('login'),
+          password: formData.get('password'),
+        };
+
+        try {
+          const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem(JWT_TOKEN_KEY, data.token)
+            auth.login = data.login;
+            router.setRoute(`/user/${data.login}`);
+
+            return true;
+          } else {
+            const {message = 'Неизвестная ошибка'} = await response.json();
+            console.log(await response.json());
+            throw new Error(message);
+          }
+
+        } catch (error) {
+          alert(error.message)
+        }
+      }
+    });
   })
 
   nav.append(buttonSignUp, buttonLogin);
